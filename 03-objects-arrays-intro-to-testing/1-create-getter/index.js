d822871453f79b985f9d74c899f6b3540daedbac
Alexey Demin <alexmelody@gmail.com>
2020-12-20 17:53:16 +0100

    Day 3: home tasks

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+	const keys = path.split('.')[Symbol.iterator](); // iterator for keys in the path
 
+	return function(obj) {
+		// function getter checks the keys recursively
+		function getter(value) {
+        	const key = keys.next();
+        	return (key.done) ? value : // last key reached, return value
+          		   (Object.keys(value).includes(key.value)) ? getter(value[key.value]) :  // key exists, continue recursion
+          		   											  value[key.value];			  // key doesn't exist, return 'undefined'
+		}
+        return getter(obj);
+	}
 }
